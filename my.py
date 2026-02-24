4efb3f635a4d58dc31f1ae594a500519499f552c
hhy <13612276628@163.com>
2026-02-24 18:00:34 +0800

    0

--- my.py
+++ my.py
@@ -14,16 +14,13 @@ print("all libs are loaded" + "-"*50)
 transform = torchvision.transforms.Compose([
     torchvision.transforms.Resize((256, 256)),
     torchvision.transforms.ToTensor()
-    #torchvision.transforms.Normalize(mean = [0.5,0.5,0.5], std = [0.5,0.5,0.5])
 ])
 device = torch.device("cpu")
 
-print("transformer and cuda loaded" + "-"*50)
-
 class MyDataset(torch.utils.data.Dataset):
     def __init__(self):
-        X = pathlib.Path("../pet_data_small/")
-        Y = pathlib.Path("../pet_data_mask_small/")
+        X = pathlib.Path("../data/")
+        Y = pathlib.Path("../data_test/")
         self.X_data = [str(i) for i in X.iterdir()]
         self.Y_data = [str(i) for i in Y.iterdir()]
         
@@ -31,18 +28,18 @@ class MyDataset(torch.utils.data.Dataset):
         return len(self.X_data)
         
     def __getitem__(self, idx):
-        return (transform(PIL.Image.open(self.X_data[idx]).convert('RGB')), transform(PIL.Image.open(self.Y_data[idx]).convert('RGB')))
+        return (transform(PIL.Image.open(self.X_data[idx]).convert('L')), transform(PIL.Image.open(self.Y_data[idx]).convert('L')))
         
 my_dataset = MyDataset()
 print("data loaded" + "-"*50)
 
-loader = torch.utils.data.DataLoader(my_dataset, shuffle = True, batch_size = 29)
+loader = torch.utils.data.DataLoader(my_dataset, shuffle = True, batch_size = 39)
 
 class net(torch.nn.Module):
     def __init__(self):
         super().__init__()
         
-        self.conv1 = torch.nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.conv1 = torch.nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
         self.bn1 = torch.nn.BatchNorm2d(32)  
         self.conv2 = torch.nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
         self.bn2 = torch.nn.BatchNorm2d(32)
@@ -65,7 +62,7 @@ class net(torch.nn.Module):
         self.bn5 = torch.nn.BatchNorm2d(32)
         self.transpose_conv3 = torch.nn.ConvTranspose2d(32, 32, kernel_size=4, stride=2, padding=1)
         self.bn6 = torch.nn.BatchNorm2d(32)
-        self.transpose_conv4 = torch.nn.ConvTranspose2d(32, 3, kernel_size=4, stride=2, padding=1)
+        self.transpose_conv4 = torch.nn.ConvTranspose2d(32, 1, kernel_size=4, stride=2, padding=1)
 
         
     def forward(self, x):
@@ -101,7 +98,7 @@ fig, ax = matplotlib.pyplot.subplots()
 x_data, y_data = [], []
 iid = 0
 
-for i in range(0, 200):
+for i in range(0, 20):
     total_loss = 0
     for id,(x,y) in enumerate(loader):
         x = x.to(device)
@@ -112,13 +109,12 @@ for i in range(0, 200):
         loss.backward()
         optimizer.step()
         total_loss += loss.item()
-        print((id + 1)*29/len(my_dataset))
         x_data.append(iid)
-        y_data.append(total_loss/29/(id + 1))
+        y_data.append(total_loss/10/(id + 1))
         ax.clear()
         ax.plot(x_data, y_data, 'b-o')
         ax.set_xlim(0, max(10, iid))
-        ax.set_ylim(0, 0.025)
+        ax.set_ylim(0, max(y_data))
         ax.set_xlabel("Time")
         ax.set_ylabel("Value")
         matplotlib.pyplot.pause(1)
@@ -130,29 +126,23 @@ torch.save(model, "model.pth")
 matplotlib.pyplot.ioff()
 matplotlib.pyplot.show()
 
+
+
 '''
 #test the input and output
 model = torch.load("model.pth", weights_only = False)
 model.eval()
-image1 = my_dataset[1][0].permute(1,2,0)
-image2 = model(my_dataset[1][0].unsqueeze(0))[0].permute(1,2,0)
-image2 = image2.detach()
-
-image2 = image2.reshape(65536, 3)
-
-newimage = torch.zeros(65536)
-i = 0
-
-while i < 256*256:
-    newimage[i] = torch.argmax(image2[i])
-    i += 1
-image2 = newimage
-image2 = image2.reshape(256,256)
-print(image2)
-
-
-fig, axes = matplotlib.pyplot.subplots(1,2)
+image1 = my_dataset[0][0].permute(1,2,0)
+image11 = my_dataset[0][1].permute(1,2,0)
+image2 = model(my_dataset[0][0].unsqueeze(0))[0].permute(1,2,0).detach()
+'''for i in image2:
+    for j in i:
+        if j != 0:
+            j *= 50
+            '''
+fig, axes = matplotlib.pyplot.subplots(1,3)
 axes[0].imshow(image1)
-axes[1].imshow(image2)
+axes[1].imshow(image11)
+axes[2].imshow(image2)
 matplotlib.pyplot.show()
 
